1b0af7ac8f2736a833ea1a7b70c443f779c65851
Carlos Jaya <cajaya1@espe.edu.ec>
2025-08-07 09:19:29 -0500

    perfilProyectoFinal

--- CODIGO/Sprint 1/ferreteriaDSA/backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/AuthController');
-
-// Ruta: POST /api/login
-router.post('/login', AuthController.login);
-
-module.exports = router;
