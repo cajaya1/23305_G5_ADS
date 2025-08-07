867908f8471af87666fad2f87153c5f1751c094f
elkinpabon <elkindres2@gmail.com>
2025-08-06 21:39:42 -0500

    Reestructuracion de carpetas base -Elkin Pabon-

--- Biblioteca de trabajo/CODIGO/Sprint 1/ferreteriaDSA/backend/src/facades/AuthFacade.js
+++
@@ -1,9 +0,0 @@
-const AuthService = require('../services/AuthService');
-
-const AuthFacade = {
-  async login(email, password) {
-    return await AuthService.login(email, password);
-  }
-};
-
-module.exports = AuthFacade;
